80426ee0249c7b2e660702d1cc1eb3c426fc6265
nakad-j <water.gate.king.5211@gmail.com>
2020-09-29 21:57:57 +0900

    test

--- roulette.js
+++ roulette.js
@@ -1,9 +1,1 @@
-// console.log("JavaScriptを実行しています");
-function clickBtn1(){
-	// 値を取得
-	const ta1 = document.form1.textarea1.value; 
-	document.getElementById("span1").textContent = ta1;
-}
-function clickBtn2(){
-	document.getElementById("span1").textContentz = "";
-}
+console.log("JavaScriptを実行しています");
